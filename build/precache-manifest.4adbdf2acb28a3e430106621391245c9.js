38fd9adac14e2bd22380e6c6d25a129466b6852c
akdombrowski <akdflyinvee73@gmail.com>
2020-06-22 03:47:02 -0500

    release build

---
+++ build/precache-manifest.4adbdf2acb28a3e430106621391245c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05051230141cdb3f40370c76aff9bd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d2910e84acc93b31ec",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "402be6f99ae435b6b631",
+    "url": "/static/js/2.3e7768ed.chunk.js"
+  },
+  {
+    "revision": "0cca9a693a58fdf1191a50afdfa7a25e",
+    "url": "/static/js/2.3e7768ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d2910e84acc93b31ec",
+    "url": "/static/js/main.f1247ea5.chunk.js"
+  },
+  {
+    "revision": "686aa18f9622428e03a2",
+    "url": "/static/js/runtime-main.0b4f3ab1.js"
+  },
+  {
+    "revision": "b15fcf979de986a30f821a3115c349b0",
+    "url": "/static/media/logo.b15fcf97.png"
+  }
+]);
