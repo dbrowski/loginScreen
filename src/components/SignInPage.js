5c49e90bdf421a7288060db88cd9dfb9c5cbae78
akdombrowski <akdflyinvee73@gmail.com>
2020-07-02 08:05:11 -0500

    Added hand button for moving sign in form

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -17,12 +17,16 @@ import NoAccount from "./InputForm/NoAccount";
 import Copyright from "./InputForm/Copyright";
 import Slider from "@material-ui/core/Slider";
 import Fab from "@material-ui/core/Fab";
+import Draggable, { DraggableCore } from "react-draggable"; // Both at the same time
+import HelpPopover from "./InputForm/HelpPopover";
+import AddIcon from "@material-ui/icons/Add";
+import PanToolIcon from "@material-ui/icons/PanTool";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
     fontWeight: 500,
-    fontColor: "white"
+    fontColor: "white",
   },
   image: {
     backgroundImage: "url(https://source.unsplash.com/random)",
@@ -49,6 +53,10 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
   },
 }));
 
@@ -81,100 +89,135 @@ export default function SignInPage({
       >
         <DragNDrop />
       </Grid>
-      <Grid
-        item
-        xs={12}
-        sm={8}
-        md={6}
-        lg={4}
-        xl={3}
-        component={Paper}
-        elevation={24}
-        square
-        style={{
-          maxHeight: "90%",
-          zIndex: 1,
-          backgroundColor: "rgba(255, 255, 255, " + opacity / 100 + ")",
-        }}
+      <Draggable
+        axis="both"
+        defaultPosition={{ x: 0, y: 0 }}
+        position={null}
+        grid={[5, 5]}
+        scale={1}
+        handle=".handle"
+        bounds="parent"
       >
-        <div
-          className={classes.paper}
+        <Grid
+          item
+          container
+          xs={12}
+          sm={8}
+          md={6}
+          lg={4}
+          xl={3}
+          component={Paper}
+          elevation={24}
+          square
+          direction="column"
+          wrap="nowrap"
+          justify="space-around"
+          alignItems="center"
           style={{
-            backgroundColor:
-              "rgba(255, 255, 255, " + (opacity - 70) / 100 + ")",
+            maxHeight: "80vh",
+            zIndex: 1,
+            backgroundColor: "rgba(255, 255, 255, " + opacity / 100 + ")",
           }}
         >
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography
-            component="h1"
-            variant="h5"
-            color={opacity / 100 < 0.4 ? "secondary" : "primary"}
-          >
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <EmailInput
-              color={opacity / 100 < 0.4 ? "secondary" : "primary"}
-              handleUsernameUpdate={handleUsernameUpdate}
-            />
-            <PasswordInput
-              color={opacity / 100 < 0.4 ? "secondary" : "primary"}
-              handlePasswordUpdate={handlePasswordUpdate}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="button"
-              data-id="user-credentials-submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={handleSubmit}
+          <Grid item height="90%">
+            <div
+              className={classes.paper}
+              style={{
+                backgroundColor:
+                  "rgba(255, 255, 255, " + (opacity - 70) / 100 + ")",
+              }}
             >
-              Sign In
-            </Button>
-            <Grid item container>
-              <Grid item xs={6}>
-                <ForgotPassword handleForgotPassword={handleForgotPassword} />
-              </Grid>
-              <Grid item xs={6}>
-                <NoAccount handleRegister={handleRegister} />
-              </Grid>
-            </Grid>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography
+                component="h1"
+                variant="h5"
+                color={opacity / 100 < 0.4 ? "secondary" : "primary"}
+              >
+                Sign in
+              </Typography>
+              <form className={classes.form} noValidate>
+                <EmailInput
+                  color={opacity / 100 < 0.4 ? "secondary" : "primary"}
+                  handleUsernameUpdate={handleUsernameUpdate}
+                />
+                <PasswordInput
+                  color={opacity / 100 < 0.4 ? "secondary" : "primary"}
+                  handlePasswordUpdate={handlePasswordUpdate}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="button"
+                  data-id="user-credentials-submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={handleSubmit}
+                >
+                  Sign In
+                </Button>
+                <Grid item container>
+                  <Grid item xs={6}>
+                    <ForgotPassword
+                      handleForgotPassword={handleForgotPassword}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <NoAccount handleRegister={handleRegister} />
+                  </Grid>
+                </Grid>
 
-            <Grid
-              container
-              item
-              direction="column"
-              height="20vh"
-              maxHeight="20vh"
-              justify="space-around"
-              style={{ height: "25vh", maxHeight: "100%" }}
-            >
-              <Grid item>
-                <Fab style={{ width: "100%" }}>
-                  <Slider
-                    value={opacity}
-                    onChange={handleChange}
-                    min={0}
-                    max={100}
-                    aria-labelledby="continuous-slider"
-                    style={{backgroundColor: "transparent"}}
-                  />
-                </Fab>
-              </Grid>
-              <Grid item>
-                <Copyright />
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Grid>
+                <Grid
+                  container
+                  item
+                  direction="column"
+                  // height="20vh"
+                  justify="space-around"
+                  wrap="nowrap"
+                  alignItems="center"
+                  style={{ maxHeight: "100%" }}
+                >
+                  <Grid item width="100%" style={{ paddingTop: "1rem" }}>
+                    <Fab style={{ width: "100%", minWidth: "20vw" }}>
+                      <Slider
+                        value={opacity}
+                        onChange={handleChange}
+                        min={0}
+                        max={100}
+                        aria-labelledby="continuous-slider"
+                        style={{ backgroundColor: "transparent" }}
+                      />
+                    </Fab>
+                  </Grid>
+                  <Grid item style={{ paddingTop: "1rem" }}>
+                    <Copyright />
+                  </Grid>
+                  <Grid
+                    item
+                    className="handle"
+                    alignContent="center"
+                    height="1vh"
+                    style={{ paddingTop: "1rem" }}
+                  >
+                    <Fab size="small" color="primary" aria-label="add">
+                      <PanToolIcon />
+                    </Fab>
+                  </Grid>
+                </Grid>
+              </form>
+            </div>
+          </Grid>
+          <Grid item justify="center" alignContent="center" height="10%" width="100%">
+            {/* */}
+            <HelpPopover />
+          </Grid>
+        </Grid>
+      </Draggable>
     </Grid>
   );
 }
